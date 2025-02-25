17b1e034eaee16736b942c8e122499cc51cdf9e2
davidvchang <dvalenzuelachang@gmail.com>
2025-02-24 22:05:59 -0700

    Change: color text DV

--- src/sections/NavBar.jsx
+++ src/sections/NavBar.jsx
@@ -3,7 +3,7 @@ import Nav from "../components/Nav"
 const NavBar = () => {
   return (
     <section className='w-full h-16 flex items-center justify-between px-20 text-white bg-[#020706]'>
-        <span className='text-4xl font-medium'>DV</span>
+        <span className='text-4xl font-semibold bg-gradient-to-r from-teal-400 to-blue-500 bg-clip-text text-transparent'>DV</span>
 
         <div className='w-fit flex gap-10'>
             <Nav link="#" text="Inicio"/>
