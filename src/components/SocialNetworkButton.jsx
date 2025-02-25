b3648e55b4956bfb9f678457a7130f4cec400d1c
davidvchang <dvalenzuelachang@gmail.com>
2025-02-24 23:29:52 -0700

    Add: home section

--- src/components/SocialNetworkButton.jsx
+++ src/components/SocialNetworkButton.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
 
-const SocialNetworkButton = () => {
+const SocialNetworkButton = ({icon}) => {
   return (
-    <div>
+    <div className='flex justify-center items-center border border-slate-700 w-fit h-fit rounded-full p-2 hover:border-emerald-600 hover:transition duration-300'>
+        {icon}
       
     </div>
   )
--- src/sections/Home.jsx
+++ src/sections/Home.jsx
@@ -1,25 +1,40 @@
 import React from 'react'
+import { Github, Linkedin, Mail } from 'lucide-react';
+import SocialNetworkButton from '../components/SocialNetworkButton';
+
 
 const Home = () => {
   return (
     <section className='flex justify-between items-center w-full'>
         {/* PRESENTATION */}
         <div className='flex flex-col text-white'>
-            <div className='flex flex-col'>
-                <span className='text-5xl font-light'>Hola, mi nombre es</span>
-                <span className='text-7xl font-semibold bg-gradient-to-r from-teal-400 to-blue-500 bg-clip-text text-transparent'>David Valenzuela</span>
-                <span className='text-2xl font-light text-slate-300'>Ing. en Sistemas Computacionales</span>
-
-                <div className='flex'>
-                    <a href="" className='w-fit h-fit px-10 py-3 rounded-full font-medium bg-gradient-to-r from-teal-400 to-blue-500 text-white'>Ver CV</a>
+            <div className='flex flex-col gap-10'>
+                <div className='flex flex-col gap-5'>
+                    <span className='border text-sm select-none w-fit h-fit px-2 border-emerald-700 text-emerald-400 font-light rounded-full'>Disponible para trabajar</span>
                     
+                    <div className='flex flex-col'>
+                        <span className='text-5xl font-light'>Hola, mi nombre es</span>
+                        <span className='text-7xl font-semibold bg-gradient-to-r from-teal-400 to-blue-500 bg-clip-text text-transparent'>David Valenzuela</span>
+                        <span className='text-2xl font-light text-slate-300'>Ing. en Sistemas Computacionales</span>
+
+                    </div>
+                </div>
+
+                <div className='flex items-center gap-7'>
+                    <a href="" className='w-fit h-fit px-12 py-3 rounded-full font-medium bg-gradient-to-r from-teal-400 to-blue-500 text-white hover:brightness-95 hover:transition duration-300'>Ver CV</a>
+                    
+                    <div className='flex gap-4'>
+                        <SocialNetworkButton icon={<Github className='w-5 h-5'/>}/>
+                        <SocialNetworkButton icon={<Linkedin className='w-5 h-5'/>}/>
+                        <SocialNetworkButton icon={<Mail className='w-5 h-5'/>}/>
+
+                    </div>
                 </div>
             </div>
         </div>
 
         {/* PHOTO */}
         <div className='w-[32rem] h-[32rem] bg-slate-100 rounded-2xl'>
-
         </div>
       
     </section>
