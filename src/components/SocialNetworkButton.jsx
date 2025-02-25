90cb14a921cf2945fa27c4240273a0f91b01a9ff
davidvchang <dvalenzuelachang@gmail.com>
2025-02-25 00:13:21 -0700

    Add: profile photo in home

--- src/components/SocialNetworkButton.jsx
+++ src/components/SocialNetworkButton.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const SocialNetworkButton = ({icon}) => {
   return (
-    <div className='flex justify-center items-center border border-slate-700 w-fit h-fit rounded-full p-2 hover:border-emerald-600 hover:transition duration-300'>
+    <div className='flex justify-center items-center border border-slate-700 w-fit h-fit rounded-full p-2 hover:border-emerald-600 hover:transition duration-300 cursor-pointer'>
         {icon}
       
     </div>
--- src/sections/Home.jsx
+++ src/sections/Home.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import { Github, Linkedin, Mail } from 'lucide-react';
 import SocialNetworkButton from '../components/SocialNetworkButton';
+import ProfilePhoto from '../assets/img/FotoPerfil.webp'
 
 
 const Home = () => {
   return (
-    <section className='flex justify-between items-center w-full'>
+    <section className='flex justify-between items-center w-full mt-16' style={{height: "calc(100vh - 64px)"}}>
         {/* PRESENTATION */}
         <div className='flex flex-col text-white'>
             <div className='flex flex-col gap-10'>
@@ -21,7 +22,7 @@ const Home = () => {
                 </div>
 
                 <div className='flex items-center gap-7'>
-                    <a href="" className='w-fit h-fit px-12 py-3 rounded-full font-medium bg-gradient-to-r from-teal-400 to-blue-500 text-white hover:brightness-95 hover:transition duration-300'>Ver CV</a>
+                    <a href="" className='w-fit h-fit px-12 py-3 rounded-full font-medium bg-gradient-to-r from-teal-400 to-blue-500 text-white hover:brightness-90 hover:transition duration-300'>Ver CV</a>
                     
                     <div className='flex gap-4'>
                         <SocialNetworkButton icon={<Github className='w-5 h-5'/>}/>
@@ -34,7 +35,8 @@ const Home = () => {
         </div>
 
         {/* PHOTO */}
-        <div className='w-[32rem] h-[32rem] bg-slate-100 rounded-2xl'>
+        <div className='w-[26rem] h-[26rem] bg-slate-100 rounded-full overflow-hidden relative'>
+            <img src={ProfilePhoto} alt="" className='w-[27rem] h-[27rem] object-contain absolute -bottom-5 -left-2'/>
         </div>
       
     </section>
