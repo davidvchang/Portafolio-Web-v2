d4cba0a97b797284116f66e615711a649fdbdbe2
davidvchang <dvalenzuelachang@gmail.com>
2025-03-25 23:17:36 -0700

    Add: project BlogSpace

--- src/sections/Projects.jsx
+++ src/sections/Projects.jsx
@@ -4,6 +4,7 @@ import PelisDev from '../assets/img/covers/PelisDev.webp'
 import Biblioteca from '../assets/img/covers/Sistema-Para-biblioteca.webp'
 import Desarrollo from '../assets/img/covers/En-desarrollo-portada.webp'
 import Ecommerce from '../assets/img/covers/Ecommerce.webp'
+import BlogSpace from '../assets/img/covers/BlogSpace.webp'
 
 import TechnologyUsed from '../components/TechnologyUsed'
 import BtnActionsProjects from '../components/BtnActionsProjects'
@@ -16,22 +17,44 @@ const Projects = () => {
         <span className='text-4xl text-center'>Proyectos</span>
 
         <div className='grid grid-cols-1 md:grid-cols-1 md:justify-items-center lg:grid-cols-2 xl:grid-cols-3 gap-y-6 pb-10'>
-            <ProjectCard image={PelisDev} title="PelisDev" description="Página web de pelicula inspirada en Cuevana, está hecha en React y estilada con Tailwind, se realizó solo la parte Front - End, se puede buscar peliculas por nombre o por categoría, así como cambiar de página." 
+            <ProjectCard image={BlogSpace} title="BlogSpace" description="Página en la cuál puedes ver todos los blogs que los usuarios han agregado, como también puedes registrarte, iniciar sesión y poder crear tus propios blogs, eliminarlos, editarlos y poder comentar otros blogs"
                 children={
                     <>
                         <TechnologyUsed name="React"/>
-                        <TechnologyUsed name="JavaScript"/>
+                        <TechnologyUsed name="TypeScript"/>
                         <TechnologyUsed name="Tailwind"/>
+                        <TechnologyUsed name="Express.js"/>
+                        <TechnologyUsed name="PostgreSQL"/>
                     </>
                 }
 
                 children2={
                     <>
-                        <BtnActionsProjects link="https://github.com/davidvchang/PelisDev" icon={<Github className='text-white w-5 h-5'/>} text="Ver repositorio"/>
-                        <BtnActionsProjects link="https://pelis-dev.netlify.app/" icon={<Link className='text-white w-5 h-5'/>} text="Live Demo"/>
+                        <BtnActionsProjects link="https://github.com/davidvchang/BlogSpace" icon={<Github className='text-white w-5 h-5'/>} text="Ver repositorio"/>
+                        {/* <BtnActionsProjects link="https://pelis-dev.netlify.app/" icon={<Link className='text-white w-5 h-5'/>} text="Live Demo"/> */}
                     </>
                 }
             />
+
+            <ProjectCard image={Ecommerce} title="TechQuality Ecommerce" description="Ecommerce para una tienda ficticia de tecnologia. Permite registrarse, iniciar sesón, agregar productos al carrito y comprar por medio de mercadopago."
+                children={
+                    <>
+                        <TechnologyUsed name="React"/>
+                        <TechnologyUsed name="TypeScript"/>
+                        <TechnologyUsed name="Tailwind"/>
+                        <TechnologyUsed name="Express.js"/>
+                        <TechnologyUsed name="PostgreSQL"/>
+                    </>
+                }
+
+                children2={
+                    <>
+                        <BtnActionsProjects link="https://github.com/davidvchang/TechQuality---Ecommerce" icon={<Github className='text-white w-5 h-5'/>} text="Ver repositorio"/>
+                        {/* <BtnActionsProjects link="https://pelis-dev.netlify.app/" icon={<Link className='text-white w-5 h-5'/>} text="Live Demo"/> */}
+                    </>
+                }
+            />
+            
             <ProjectCard image={Biblioteca} title="Sistema para biblioteca" description="Sistema fullstack para una biblioteca en la cual permita prestar libros, así como agregar nuevos libros, poder editar, eliminar cada libro, de la misma forma para los clientes."
                 children={
                     <>
@@ -68,25 +91,23 @@ const Projects = () => {
                     </>
                 }
             />
-            <ProjectCard image={Ecommerce} title="TechQuality Ecommerce" description="Ecommerce para una tienda ficticia de tecnologia. Permite registrarse, iniciar sesón, agregar productos al carrito y comprar por medio de mercadopago."
+
+            <ProjectCard image={PelisDev} title="PelisDev" description="Página web de pelicula inspirada en Cuevana, está hecha en React y estilada con Tailwind, se realizó solo la parte Front - End, se puede buscar peliculas por nombre o por categoría, así como cambiar de página." 
                 children={
                     <>
                         <TechnologyUsed name="React"/>
-                        <TechnologyUsed name="TypeScript"/>
+                        <TechnologyUsed name="JavaScript"/>
                         <TechnologyUsed name="Tailwind"/>
-                        <TechnologyUsed name="Express.js"/>
-                        <TechnologyUsed name="PostgreSQL"/>
                     </>
                 }
 
                 children2={
                     <>
-                        <BtnActionsProjects link="https://github.com/davidvchang/TechQuality---Ecommerce" icon={<Github className='text-white w-5 h-5'/>} text="Ver repositorio"/>
-                        {/* <BtnActionsProjects link="https://pelis-dev.netlify.app/" icon={<Link className='text-white w-5 h-5'/>} text="Live Demo"/> */}
+                        <BtnActionsProjects link="https://github.com/davidvchang/PelisDev" icon={<Github className='text-white w-5 h-5'/>} text="Ver repositorio"/>
+                        <BtnActionsProjects link="https://pelis-dev.netlify.app/" icon={<Link className='text-white w-5 h-5'/>} text="Live Demo"/>
                     </>
                 }
             />
-
         </div>
       
     </section>
